5c2872bdd2c639f8fe7ecd0d0f2834cfec472e08
Luis Mesa <luis.mesa@gse.com.co>
2024-04-17 09:24:22 -0500

    feature/lm/android_14_fix - Se realiza ajustes de nuevas poiliticas de google.

--- LuguApp/build/generated/source/buildConfig/debug/com/lamesa/lugu/BuildConfig.java
+++ LuguApp/build/generated/source/buildConfig/debug/com/lamesa/lugu/BuildConfig.java
@@ -7,8 +7,8 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.lugumusic.lofi";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 14;
-  public static final String VERSION_NAME = "1.3.4 beta";
+  public static final int VERSION_CODE = 15;
+  public static final String VERSION_NAME = "1.3.5 beta";
   // Field from build type: debug
   public static final boolean LOG_DEBUG = true;
 }
